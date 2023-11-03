21aa44ee18086e26f00802a912539b6166a25211
Ksaiuttej <ksaiuttej@gmail.com>
2023-11-03 10:47:23 +0530

    added link module to banner jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Banner = () => {
   return (
@@ -7,9 +8,9 @@ const Banner = () => {
         <h1>Don't Know What to Cook?</h1>
         <p>Explore a world of culinary inspiration with EatsVoyage.Say goodbye to mealtime dilemmas and hello to flavorful experiences. Let's embark on a culinary journey together!"</p>
         <div className="btn-box">
-          <a href="/random" class="search-link-btn">
+          <Link to="/random">
             <button className="sub">Random Recipes &nbsp;&nbsp;<span><i class="fa-sharp fa-solid fa-arrow-right">  </i></span></button>
-          </a>
+          </Link>
 
           <div className="inner-border"></div>
         </div>
