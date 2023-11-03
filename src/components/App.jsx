6db106aa7f1748f3103b984ba01c80d671c72bd7
Ksaiuttej <ksaiuttej@gmail.com>
2023-11-03 10:22:29 +0530

    added Hash

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import Random from './Random';
 import Search from './Search';
 import ItemDetail from './ItemDetail';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
